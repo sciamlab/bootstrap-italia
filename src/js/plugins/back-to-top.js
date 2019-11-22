12f489035cdb34a2dc951affbd9b31d7eb1ec17e
Tommaso Biondo <tommasobiondo@gmail.com>
2019-11-22 15:17:45 +0100

    update back to top function

--- src/js/plugins/back-to-top.js
+++ src/js/plugins/back-to-top.js
@@ -1,13 +1,14 @@
 $(function() {
-  const $backToTopElements = $('a[data-attribute*="back-to-top"]')
+
   $(window).on('scroll', function() {
+    const $backToTopElements = $('a[data-attribute*="back-to-top"]')
     $backToTopElements.toggleClass(
       'back-to-top-show',
       $backToTopElements.length && $(this).scrollTop() >= 100
     )
   })
 
-  $backToTopElements.on('click', function() {
+  $(document).on('click','a[data-attribute*="back-to-top"]', function() {
     $('body,html').animate({ scrollTop: 0 }, 800)
   })
 })
